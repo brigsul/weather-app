e1304ec3b21b39987b7681e0d8ba664531133d32
Brian Sullivan <bri.g.sullivan@gmail.com>
2016-10-20 23:27:41 -0400

    working 24-hour forecast and icons, v1.0!

--- main.js
+++ main.js
@@ -17,39 +17,96 @@ function setLocation (lat, lon) {
   console.log(urlForecast);
 }
 
+function setIcon (ID) {
+  switch(ID) {
+    case 800:
+        if (7 < hour && hour < 19) {
+          $('#weatherIcon').html("<img src='icons\\clear-day.png'/>");
+        }
+        else {
+          $('#weatherIcon').html("<img src='icons\\clear-night.png'/>");
+        }
+        break;
+    case 801:
+        if (7 < hour && hour < 19) {
+          $('#weatherIcon').html("<img src='icons\\cloudy-day.png'/>");
+        }
+        else {
+          $('#weatherIcon').html("<img src='icons\\cloudy-night.png'/>");
+        }
+        break;
+    case 802:
+    case 803:
+    case 804:
+        if (7 < hour && hour < 19) {
+          $('#weatherIcon').html("<img src='icons\\cloudy.png'/>");
+        }
+        else {
+          $('#weatherIcon').html("<img src='icons\\cloudy-night.png'/>");
+        }
+        break;
+    case 500:
+    case 501:
+    case 502:
+    case 503:
+    case 504:
+    case 511:
+    case 520:
+    case 521:
+    case 522:
+    case 531:
+        if (7 < hour && hour < 19) {
+          $('#weatherIcon').html("<img src='icons\\rainy-day.png'/>");
+        }
+        else {
+          $('#weatherIcon').html("<img src='icons\\rainy-night.png'/>");
+        }
+        break;
+    case 600:
+    case 601:
+    case 602:
+    case 611:
+    case 612:
+    case 615:
+    case 616:
+    case 620:
+    case 621:
+    case 622:
+        if (7 < hour && hour < 19) {
+          $('#weatherIcon').html("<img src='icons\\snowy-day.png'/>");
+        }
+        else {
+          $('#weatherIcon').html("<img src='icons\\snowy-night.png'/>");
+        }
+        break;
+    case 200:
+    case 201:
+    case 202:
+    case 210:
+    case 211:
+    case 212:
+    case 221:
+    case 230:
+    case 231:
+    case 232:
+        $('#weatherIcon').html("<img src='icons\\tstorms.png'/>");
+        break;
+  }
+}
+
 function setWeather (response) {
   var cityName = response.name;
   var curTemp = Math.round(response.main.temp);
   var curCond = response.weather[0].main;
   var curHum = response.main.humidity;
+  var curID = response.weather[0].id;
 
   $('#location').html(cityName);
   $('#t1').html(curTemp+"&#8457;");
   $('#c1').html(curCond);
   $('#hum1').html("Humidity: "+curHum+"%");
 
-  console.log(hour);
-
-  if (curCond == "Clear") {
-    if (7 < hour && hour < 19) {
-      $('#weatherIcon').html("<img src='icons\\clear-day.png'/>");
-    }
-    else {
-      $('#weatherIcon').html("<img src='icons\\clear-night.png'/>");
-    }
-  }
-  else if (curCond == "Cloudy") {
-
-  }
-  else if (curCond == "Rain") {
-
-  }
-  else if (curCond == "Sonw") {
-
-  }
-  else if (curCond == "Storms") {
-
-  }
+  setIcon(curID);
 
   console.log(response);
 }
